169bfaacef5e98a811330048e7db2e1cd57f642d
karomag <roma-novikov@mail.ru>
2019-12-22 01:24:03 +0300

    update lint style

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,5 +5,6 @@ import prompt
 
 def run():
     """Запрашивает имя пользователя и выводит строку приветсвия."""
+    print('Welcome to the Brain Games!\n')
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
