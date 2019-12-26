787671a951d68b2f1a79af6545d65406e8b5900b
karomag <roma-novikov@mail.ru>
2019-12-26 14:43:37 +0300

    Added game Prime.

---
+++ brain_games/games/prime.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""Games Prime."""
+
+from brain_games.engine import random_number
+
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def correct_answer(number):
+    """Return correct answer.
+
+    Args:
+        number (int): number for checking.
+
+    Returns:
+         (str): 'yes' if number is prime else 'no'
+    """
+    if number % 2 == 0:
+        return 'yes' if number == 2 else 'no'
+    division = 3
+    while division ** 2 <= number and number % division != 0:
+        division += 2
+    return 'yes' if division ** 2 > number else 'no'
+
+
+def get_game_task():
+    """Return parameters for task.
+
+    Returns:
+        (tuple): (question, correct_answer).
+    """
+    question_number = random_number()
+    return [question_number], correct_answer(question_number)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Brain games: Prime."""
+
+from brain_games.engine import run
+from brain_games.games import prime
+
+
+def main():
+    """Run game."""
+    run(prime)
+
+
+if __name__ == '__main__':
+    main()
